99e698886770fee9d478cd256e2c027710a3eb17
lllmaclll <makky1593@gmail.com>
2024-12-04 21:59:35 +0700

    workshop day 3

---
+++ app/backoffice/profile/page.tsx
@@ -0,0 +1,99 @@
+'use client'
+import { config } from '../../config'
+import axios from 'axios'
+import React, { useState } from 'react'
+import Swal from 'sweetalert2'
+
+function ProfilePage() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const handleSave = async () => {
+        if (username == '') {
+            Swal.fire({
+                title: 'กรุณาระบุ Username',
+                icon: 'error'
+            })
+            return
+        }
+
+        if (password !== '' && confirmPassword !== '') {
+            if (password !== confirmPassword) {
+                Swal.fire({
+                    title: 'รหัสผ่านไม่ตรงกัน',
+                    icon: 'error'
+                })
+                return
+            }
+        }
+
+        try {
+            const payload = {
+                username: username,
+                password: password
+            }
+
+            const headers = {
+                'Authorization': `Bearer ${localStorage.getItem(config.tokenKey)}`
+            }
+
+            const response = await axios.put(`${config.apiUrl}/api/user/update`, payload, {
+                headers: headers
+            })
+
+            if (response.data.message == 'success') {
+                Swal.fire({
+                    title: 'บันทึกข้อมูลสำเร็จ',
+                    icon: 'success',
+                    timer: 1000
+                })
+            }
+
+        } catch (error: any) {
+            Swal.fire({
+                title: 'มีข้อผิดพลาด',
+                icon: 'error',
+                text: error.message
+            })
+        }
+    }
+
+  return (
+    <div className='card'>
+        <h1>Profile</h1>
+        <div className='card-body'>
+            <div>Username</div>
+            <input 
+                type="text" 
+                className='form-control' 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+            />
+
+            <div className='mt-5'>Password (หากไม่ต้องการเปลี่ยน ให้ปล่อยว่าง)</div>
+            <input 
+                type="password" 
+                className='form-control' 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+            />
+
+            <div className='mt-5'>ยืนยัน Password ใหม่ (หากไม่ต้องการเปลี่ยน ให้ปล่อยว่าง)</div>
+            <input 
+                type="password" 
+                className='form-control' 
+                value={confirmPassword} 
+                onChange={(e) => setConfirmPassword(e.target.value)} 
+            />
+
+            <button className='btn-primary mt-5' onClick={handleSave}>
+                <i className='fa-solid fa-check mr-3'></i>
+                <span>บันทึก</span>
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default ProfilePage
--- app/components/top-nav.tsx
+++ app/components/top-nav.tsx
@@ -1,6 +1,41 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import Swal from 'sweetalert2'
+import { config } from '../config'
+import { useRouter } from 'next/navigation'
 
 function TopNav() {
+    const [name, setName] = useState('')
+    const [level, setLevel] = useState('')
+    const router = useRouter()
+
+    const handleLogout = async () => {
+        const button = await Swal.fire({
+            title: 'ออกจากระบบ',
+            text: 'คุณต้องการออกจากระบบ',
+            icon: 'question',
+            showCancelButton: true,
+            showConfirmButton: true
+        })
+
+        if (button.isConfirmed) {
+            localStorage.removeItem(config.tokenKey)
+            localStorage.removeItem('bun_service_name')
+            localStorage.removeItem('bun_service_level')
+
+            router.push('/')
+        }
+    }
+
+    const handleProfile = async () => {
+        router.push('/backoffice/profile')
+    }
+
+    useEffect(() => {
+        setName(localStorage.getItem('bun_service_name') || '')
+        setLevel(localStorage.getItem('bun_service_level') || '')
+    }, [])
+
   return (
     <nav className='bg-gray-800 shadow-sm'>
         <div className='mx-auto px-6'>
@@ -9,7 +44,17 @@ function TopNav() {
                     <h1 className='text-xl font-bold'>Bun Service 2025</h1>
                 </div>
                 <div className='flex items-center'>
-                    <span className='text-gray-200'>Admin</span>
+                    <span className='text-gray-200'>{name}</span>
+                    <span className='text-indigo-400 ml-5 font-bold'>( {level} )</span>
+
+                    <button className='bg-indigo-500 text-white px-4 py-2 rounded-md ml-5 hover:bg-indigo-600' onClick={handleProfile}>
+                        <i className='fa-solid fa-user mr-3'></i>
+                        <span>Profile</span>
+                    </button>
+                    <button className='bg-red-500 text-white px-4 py-2 rounded-md ml-2 hover:bg-red-600' onClick={handleLogout}>
+                        <i className='fa-solid fa-right-from-bracket mr-3'></i>
+                        <span>Logout</span>
+                    </button>
                 </div>
             </div>
         </div>
